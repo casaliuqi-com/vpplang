44410e4378e06416bb32a2389cc76dcf3b93faeb
3442853561 <21147967+3442853561@users.noreply.github.com>
2019-07-08 00:24:32 +0800

    Update vlang_src_load.rs

--- src/vlang_src_load.rs
+++ src/vlang_src_load.rs
@@ -1,9 +1,11 @@
 #[inline]
+#[cfg(target_os = "windows")]
 pub(crate) fn keywords_v<'a>() -> &'a str {
-    if cfg!(windows) {
-        include_str!(r"vlang_src\keywords.v")
-    } else {
-        include_str!(r"vlang_src/keywords.v")
-    }
+    include_str!(r"vlang_src\keywords.v")
 }
 
+#[inline]
+#[cfg(not(target_os = "windows"))]
+pub(crate) fn keywords_v<'a>() -> &'a str {
+    include_str!(r"vlang_src/keywords.v")
+}
